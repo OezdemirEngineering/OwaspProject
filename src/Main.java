7dc593ab86543640154b61705127cc24773f0d15
Caglar Özdemir <c.oezdemir@oeeng.de>
2026-01-07 20:38:07 +0100

    change

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main(String[] args) {
-    // Simulierter "User Input" (für Demo)
-    String userInput = args.length > 0 ? args[0] : "echo hello";
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String userInput = args.length > 0 ? args[0] : "echo hello";
 
-    IO.println(String.format("Hello and welcome!"));
+        System.out.println("Hello and welcome!");
 
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
-    }
-
-    // ❌ Absichtlich unsicher: Command Injection / Dangerous exec
-    try {
+        // absichtlich unsicher (typischer Trigger)
         Runtime.getRuntime().exec(userInput);
-    } catch (Exception e) {
-        IO.println("Exec failed: " + e.getMessage());
     }
 }
